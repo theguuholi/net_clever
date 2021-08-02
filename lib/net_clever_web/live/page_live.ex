85d41304efd038ec478216ec10b2fdc2a0b9ffa9
Gustavo <g.92oliveira@gmail.com>
2021-08-02 18:27:44 -0300

    refactor pages

--- lib/net_clever_web/live/page_live.ex
+++ lib/net_clever_web/live/page_live.ex
@@ -5,4 +5,9 @@ defmodule NetCleverWeb.PageLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  @impl true
+  def render(assigns) do
+    Phoenix.View.render(NetCleverWeb.PageView, "index.html", assigns)
+  end
 end
