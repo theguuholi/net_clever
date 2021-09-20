7943ea8f5802b69d268f43acc1ed03be2d7ce55b
Gustavo <g.92oliveira@gmail.com>
2021-09-20 20:59:52 -0300

    use stateful component

--- lib/net_clever_web/live/stores/store_live.ex
+++ lib/net_clever_web/live/stores/store_live.ex
@@ -8,31 +8,9 @@ defmodule NetCleverWeb.StoreLive do
     {:ok, socket, temporary_assigns: [stores: []]}
   end
 
+  @spec get_categories :: any
   def get_categories do
     Stores.get_categories()
-  end
-
-  defp load(socket, params) do
-    page = String.to_integer(params["page"] || "1")
-    per_page = String.to_integer(params["per_page"] || "5")
-    paginate = %{page: page, per_page: per_page}
-
-    sort_by = (params["sort_by"] || "name") |> String.to_atom()
-    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
-    sort_options = %{sort_by: sort_by, sort_order: sort_order}
-    stores = Stores.list_stores_with_filters(paginate: paginate, sort: sort_options)
-
-    params =
-      [
-        stores: stores,
-        options: Map.merge(paginate, sort_options),
-        name: nil, loading: false, matches: [], page: page, per_page: per_page
-      ]
-
-    assign(
-      socket,
-      params
-    )
   end
 
   @impl true
@@ -42,8 +20,7 @@ defmodule NetCleverWeb.StoreLive do
 
   @impl true
   def handle_params(params, _uri, socket) do
-    socket = load(socket, params)
-    {:noreply, socket}
+    {:noreply, assign(socket, params: params)}
   end
 
   @impl true
@@ -53,62 +30,9 @@ defmodule NetCleverWeb.StoreLive do
   end
 
   @impl true
-  def handle_info({:search_by_name, name}, socket) do
-    :timer.sleep(1000)
-    stores = Stores.list_stores_by_name(name)
-
-    socket =
-      case stores do
-        [] ->
-          socket
-          |> put_flash(:info, "Nao existe lojas com: \"#{name}\"")
-          |> assign(stores: stores, loading: false)
-
-        stores ->
-          assign(socket, stores: stores, loading: false)
-      end
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("name-search", %{"name" => name}, socket) do
-    send(self(), {:search_by_name, name})
-    {:noreply, assign(socket, stores: [], loading: true, name: name)}
-  end
-
-
-  @impl true
   def handle_info({:store_created, store}, socket) do
     socket = put_flash(socket, :info, "Loja #{store.name} foi criada!")
     {:noreply, socket}
   end
 
-  @impl true
-  def handle_event("filter-category", %{"category" => category}, socket) do
-    stores = Stores.list_stores_with_filters(category: category)
-    {:noreply, assign(socket, stores: stores)}
-  end
-
-  @impl true
-  def handle_event("suggest-store", %{"name" => name}, socket) do
-    matches = Stores.list_suggest_stores_by_name(name)
-    {:noreply, assign(socket, matches: matches)}
-  end
-
-  @impl true
-  def handle_event("select-per-page", %{"per-page" => per_page}, socket) do
-    per_page = String.to_integer(per_page)
-    paginate = %{page: socket.assigns.options.page, per_page: per_page}
-    socket =
-      push_patch(socket,
-        to: Routes.store_path(socket, :index,
-        page: paginate.page,
-        per_page: per_page,
-        sort_by: socket.assigns.options.sort_by,
-        sort_order: socket.assigns.options.sort_order)
-      )
-
-    {:noreply, socket}
-  end
 end
--- lib/net_clever_web/live/stores/table_component.ex
+++ lib/net_clever_web/live/stores/table_component.ex
@@ -1,9 +1,89 @@
 defmodule NetCleverWeb.Stores.TableComponent do
   use NetCleverWeb, :live_component
+  alias NetClever.Stores
+
+  @impl true
+  def update(%{params: params}, socket) do
+    page = String.to_integer(params["page"] || "1")
+    per_page = String.to_integer(params["per_page"] || "5")
+    paginate = %{page: page, per_page: per_page}
+
+    sort_by = (params["sort_by"] || "name") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+    sort_options = %{sort_by: sort_by, sort_order: sort_order}
+    stores = Stores.list_stores_with_filters(paginate: paginate, sort: sort_options)
+
+    params = [
+      stores: stores,
+      options: Map.merge(paginate, sort_options),
+      name: nil,
+      loading: false,
+      matches: [],
+      page: page,
+      per_page: per_page
+    ]
+
+    {:ok, assign(socket, params)}
+  end
 
   @impl true
   def render(assigns) do
     Phoenix.View.render(NetCleverWeb.StoreView, "table.html", assigns)
   end
+
+
+  @impl true
+  def handle_event("filter-category", %{"category" => category}, socket) do
+    stores = Stores.list_stores_with_filters(category: category)
+    {:noreply, assign(socket, stores: stores)}
+  end
+
+  @impl true
+  def handle_event("select-per-page", %{"per-page" => per_page}, socket) do
+    per_page = String.to_integer(per_page)
+    paginate = %{page: socket.assigns.options.page, per_page: per_page}
+    socket =
+      push_patch(socket,
+        to: Routes.store_path(socket, :index,
+        page: paginate.page,
+        per_page: per_page,
+        sort_by: socket.assigns.options.sort_by,
+        sort_order: socket.assigns.options.sort_order)
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("suggest-store", %{"name" => name}, socket) do
+    matches = Stores.list_suggest_stores_by_name(name)
+    {:noreply, assign(socket, matches: matches)}
+  end
+
+  @impl true
+  def handle_info({:search_by_name, name}, socket) do
+    :timer.sleep(1000)
+    stores = Stores.list_stores_by_name(name)
+
+    socket =
+      case stores do
+        [] ->
+          socket
+          |> put_flash(:info, "Nao existe lojas com: \"#{name}\"")
+          |> assign(stores: stores, loading: false)
+
+        stores ->
+          assign(socket, stores: stores, loading: false)
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("name-search", %{"name" => name}, socket) do
+    send(self(), {:search_by_name, name})
+    {:noreply, assign(socket, stores: [], loading: true, name: name)}
+  end
+
 
 end
