e846ce052f02d1c9ffa0e61c7e6a8f77be623adf
Gustavo <g.92oliveira@gmail.com>
2021-09-23 20:54:07 -0300

    fix tests

--- test/net_clever_web/live/stores/store_live_test.exs
+++ test/net_clever_web/live/stores/store_live_test.exs
@@ -6,6 +6,49 @@ defmodule NetCleverWeb.Stores.StoreLiveTest do
   describe "test store" do
     setup :register_and_log_in_user
 
+    test "test name-search", %{conn: conn} do
+      {:ok, store} = NetClever.StoresFixtures.create_store()
+
+      {:ok, view, _html} = live(conn, "/stores")
+
+      rendered =
+        view
+        |> form("#form-suggest-store", %{name: "kdfsjksdljk"})
+        |> render_submit()
+
+      assert rendered =~ "Loading..."
+      refute has_element?(view, "#store-#{store.id}", "Lagoinha")
+      send(view.pid, {:search_by_name, "Lag"})
+      assert has_element?(view, "#store-#{store.id}", "Lagoinha")
+
+      view
+      |> form("#form-suggest-store", %{name: "kdfsjksdljk"})
+      |> render_submit()
+
+      assert has_element?(view, "#info-alert", "kdfsjksdljk")
+    end
+
+    test "test create store", %{conn: conn} do
+
+      {:ok, view, _html} = live(conn, "/stores")
+      {:ok, store} = NetClever.StoresFixtures.create_store()
+
+      send(view.pid, {:store_created, store})
+      assert has_element?(view, "#info-alert", "Loja #{store.name} foi criada!")
+    end
+
+    test "test store suggestion", %{conn: conn} do
+      {:ok, _store} = NetClever.StoresFixtures.create_store()
+
+      {:ok, view, _html} = live(conn, "/stores")
+
+      view
+      |> form("#form-suggest-store", %{name: "Lag"})
+      |> render_change()
+
+      assert has_element?(view, "#matches", "Lagoinha")
+    end
+
     test "check if store exist", %{conn: conn} do
       {:ok, store} = NetClever.StoresFixtures.create_store()
 
